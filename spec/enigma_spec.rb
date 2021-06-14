958ce507b57cf476c80eb8f91680a18dd05a6f7d
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-06-14 13:46:14 -0600

    Added rough draft of tests to write and run

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Enigma do
       expect(enigma).to be_an_instance_of(Enigma)
     end
 
-    it 'encrypt' do
+    it 'can encrypt a message with a key and a date' do
 
       result = {encryption: "keder ohulw",
                 key: "02715",
@@ -21,13 +21,70 @@ RSpec.describe Enigma do
       expect(enigma.encrypt("hello world", "02715", "040895")).to eq(result)
     end
 
-    it 'decrypt' do
+    it 'can decrypt a message with a key and a date' do
 
       result = { decrypttion: "hello world",
                 key: "02715",
                 date:"040895"
               }
       expect(enigma.decrypt("keder ohulw","027715","040895")).to eq(result)
+    end
+
+    context 'Student written tests' do
+      before(each) do
+
+      end
+
+      it 'can generate a key with leading zeroes' do
+
+        expect().to eq()
+      end
+
+      it 'can appropriately assign shift key randomly generated numbers indices' do
+
+        expect().to eq()
+      end
+
+      it 'can properly tranforms date into offsets with appropriately assigned indices ' do
+
+        expect().to eq()
+      end
+
+      it 'encryption shifting returning as expected' do
+
+        expect().to eq()
+      end
+
+      it 'will always restart the shift cycle at the 5th character' do
+
+        expect().to eq()
+      end
+
+      it 'accepts and returns upcased letters lowercased' do
+
+        expect().to eq()
+      end
+
+      it 'returns foreign characters as itself' do
+
+        expect().to eq()
+      end
+
+      it 'can retrieve decryption key' do
+
+        expect().to eq()
+      end
+
+      it 'can retrieve decryption date' do
+
+        expect().to eq()
+      end
+
+      it 'decryption is returning as expected' do
+
+        expect().to eq()
+      end
+
     end
   end
 end
