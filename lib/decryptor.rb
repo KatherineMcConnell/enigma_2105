e3c70f7b693c04ec711130254197f21f8cb386cc
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-06-15 17:05:23 -0600

    Pseudocoded Responsibilities, input requires, began hash return config

---
+++ lib/decryptor.rb
@@ -0,0 +1,13 @@
+require 'time'
+require 'helper_methods'
+class Decryptor
+  include HelperMethods
+  #Responsibilities:
+
+  # Setting Decrypt hash keys
+  # =>  {
+  #      :decryption => "message",
+  #      :key => "5randos",
+  #      :date => "ddmmyy"
+  #    }
+end
