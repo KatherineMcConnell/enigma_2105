15d958c1e518e802c162469121f5400ce5474307
KatherineMcConnell <mcconnell.katherine01@gmail.com>
2021-06-15 17:07:13 -0600

    Edited Describe blocks per given Interaction Pattern

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -2,7 +2,8 @@ require 'time'
 require './lib/enigma'
 
 RSpec.describe Enigma do
-  context 'Interaction Pattern' do
+
+  Describe 'Interaction Pattern: EncryptTests' do
     before(:each) do
       enigma = Enigma.new
     end
@@ -12,31 +13,63 @@ RSpec.describe Enigma do
       expect(enigma).to be_an_instance_of(Enigma)
     end
 
-    it 'can encrypt a message with a key and a date and return hash' do
+    it 'can encrypt with only 1 argument, a message, and return hash' do
 
-      result = {encryption: "keder ohulw",
+      expect().to eq()
+    end
+
+    it 'can encrypt with 2 arguements, a message and key, and return hash' do
+      #mock & stub todays date
+      encrypted = enigma.encrypt("hello world", "02715")
+
+      expect(encrypted).to eq(####)
+    end
+
+    it 'can encrypt with 2 arguments, a message and date, and return hash' do
+      ### generates random key and uses todays date
+      ### mock & stub todays date
+
+      expect(enigma.encrypt("hello world")).to eq()
+    end
+
+    it 'can encrypt with 3 arguments, a message, key, and date, and return hash' do
+
+      expected = {encryption: "keder ohulw",
                 key: "02715",
                 date: "040895"
               }
 
-      expect(enigma.encrypt("hello world", "02715", "040895")).to eq(result)
+      expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+    end
+  end
+
+
+  Describe'Interaction Pattern: DecryptionTests' do
+    before(:each) do
+      enigma = Enigma.new
     end
 
-    it 'can decrypt a message with a key and a date and return hash' do
+    it 'can decrypt with 2 arguments, a ciphertext and key, and return hash' do
+      #mock & stub todays date
 
-      result = { decryption: "hello world",
+      expect(enigma.decrypt(encrypted[:encryption], "02715")).to eq(###)
+    end
+
+    it 'can decrypt with 3 arguments, a ciphertext, key, and date, and return hash' do
+
+      expected = { decryption: "hello world",
                 key: "02715",
                 date:"040895"
               }
 
-      expect(enigma.decrypt("keder ohulw","027715","040895")).to eq(result)
+      expect(enigma.decrypt("keder ohulw","027715","040895")).to eq(expected)
     end
 
-    it 'can encrypt a message with a key and TODAYS date' do
-      return = #
-
-      expect().to eq()
-    end
+    # it 'can encrypt a message with a key and TODAYS date' do
+    #   return = #
+    #
+    #   expect().to eq()
+    # end
     #
     # context 'Student written tests' do
     #   before(each) do
